8903beee5cd8d174dbc0c517a49a4435d34cc79f
Nam Huyy <101095378+NamHuyNguyenKhac@users.noreply.github.com>
2024-04-03 21:13:44 +0700

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,6 +1,9 @@
 def calculate_total_price(quantity, customer_type):
-    if quantity < 0:
-        print("Invalid: Số lượng sản phẩm không hợp lệ")
+    # Check quantity là int và customer_type là bool
+    if not isinstance(quantity, int) or not isinstance(customer_type, bool):
+        return None
+    
+    if quantity <= 0:
         return None
     # Tính tổng tiền trước khi áp dụng chiết khấu
     total_price = quantity * 10
@@ -14,7 +17,7 @@ def calculate_total_price(quantity, customer_type):
         discount = 0
     
     # Áp dụng chiết khấu cho loại khách hàng
-    if customer_type == "Khách VIP":
+    if customer_type == True:
         discount += 0.1
     
     # Tính tổng tiền sau khi áp dụng chiết khấu
@@ -23,14 +26,17 @@ def calculate_total_price(quantity, customer_type):
     return total_price
 
 # Test cases
-test_cases = [
-    (-2, "Khách VIP"),   # Số lượng sản phẩm ít hơn 10, khách hàng thường
-    (12, "Khách thường"),  # Số lượng sản phẩm từ 10-20, khách hàng thường
-    (22, "Khách VIP"),  # Số lượng sản phẩm lớn hơn 20, khách hàng thường
-    (2, "Khách VIP")  # Số lượng sản phẩm lớn hơn 20, khách hàng VIP
+test_cases_table = [
+    (True, 21),   # Số lượng sản phẩm ít hơn 10, khách hàng thường
+    (12, False),  # Số lượng sản phẩm từ 10-20, khách hàng thường
+    (22, True),  # Số lượng sản phẩm lớn hơn 20, khách hàng thường
+    (22, False)  # Số lượng sản phẩm lớn hơn 20, khách hàng VIP
 ]
 
 # Kiểm thử các test case và in kết quả
-for quantity, customer_type in test_cases:
+for i, (quantity, customer_type) in enumerate(test_cases_table, start=1):
     total_price = calculate_total_price(quantity, customer_type)
-    print(f"Số lượng sản phẩm: {quantity}, Loại khách hàng: {customer_type}, Tổng tiền: ${total_price}")
+    if total_price is None:
+        print(f"Test Case R{i}: Invalid input: Quantity={quantity}, CustomerType={customer_type}")
+    else:
+        print(f"Test Case R{i}: Quantity: {quantity}, CustomerType: {customer_type}, Total Price: ${total_price}")
