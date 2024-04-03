6937639011d1cf85f62bf859c0379741c275b045
Nam Huyy <101095378+NamHuyNguyenKhac@users.noreply.github.com>
2024-04-04 01:17:46 +0700

    Update KiemThuDongDieuKhien.py

--- KiemThuDongDieuKhien/KiemThuDongDieuKhien.py
+++ KiemThuDongDieuKhien/KiemThuDongDieuKhien.py
@@ -1,6 +1,6 @@
 def calculate_total_price(quantity, customer_type):
     # Check quantity là int và customer_type là bool
-    if not isinstance(quantity, int) or not isinstance(customer_type, bool):
+    if not isinstance(quantity, int) or not isinstance(customer_type, bool) or quantity > 2147483647:
         return None
     
     if quantity <= 0:
@@ -26,22 +26,19 @@ def calculate_total_price(quantity, customer_type):
     return total_price
 
 # Test cases
-test_cases_table = [
-    (True, 21),     # Biến đầu vào k hợp lệ
-    (0, False),     # Số lượng sản phẩn <= 0, Khách thường
-    (5, False),     # Số lượng sản phẩm từ 5-10, khách hàng thường
-    (15, False),    # Số lượng sản phẩm từ 10-20, khách hàng thường
-    (25, False),    # Số lượng sản phẩm hơn 20, khách hàng thường
-    (-1, True),     # Số lượng sản phẩn <= 0, Khách VIP
-    (4, True),      # Số lượng sản phẩm 5-10, khách hàng VIP
-    (14, True),     # Số lượng sản phẩm 10-20, khách hàng VIP
-    (24, True),     # Số lượng sản phẩm lớn hơn 20, khách hàng VIP
+test_cases = [
+    (-2, True),   # Số lượng sản phẩm ít hơn 10, khách hàng thường
+    (12, False),  # Số lượng sản phẩm từ 10-20, khách hàng thường
+    (22, True),  # Số lượng sản phẩm lớn hơn 20, khách hàng thường
+    (2, True)  # Số lượng sản phẩm lớn hơn 20, khách hàng VIP
 ]
 
+print("Kiểm thử C2")
 # Kiểm thử các test case và in kết quả
-for i, (quantity, customer_type) in enumerate(test_cases_table, start=1):
+for i, (quantity, customer_type) in enumerate(test_cases, start=1):
     total_price = calculate_total_price(quantity, customer_type)
     if total_price is None:
         print(f"Test Case R{i}: Invalid input: Quantity={quantity}, CustomerType={customer_type}")
     else:
         print(f"Test Case R{i}: Quantity: {quantity}, CustomerType: {customer_type}, Total Price: ${total_price}")
+
